75b042595c4ccf9c2641ad38ef732874b1c8a99e
haashirmemon <haashirmemon6@gmail.com>
2024-03-05 03:36:25 -0800

    file

---
+++ app.js
@@ -0,0 +1,21 @@
+var input=document.querySelector("#input1")
+var input=document.querySelector("#input2")
+var input=document.querySelector("#input3")
+var input=document.querySelector("#input4")
+var totalMarks = 400
+
+var percentage =(obtainedmarks /totalmarks * 100)
+var totalmarks =( input.value ,input.value ,input.value,input.value)
+var obtainedMarks=(input.value+input.value+input.value+input.value)
+
+function percentageCal(percentage){
+   
+    console.log(input.value)
+    console.log(input.value)
+    console.log(input.value)
+    console.log(input.value)
+   
+}
+percentageCal()
+
+getinput.value = "percentagecal"
